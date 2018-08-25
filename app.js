4be6c710b835cda520d7299a778cbe8d1dd879df
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-08-25 11:10:45 +0300

    add html

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static('public'));
 
-app.listen(8000, function () {
-  console.log('Express запущен на http://localhost:8000\nнажмите CTRL+C для завершения');
-});
+app.listen(process.env.PORT || port, () => console.log(`Express запущен на http://localhost:${port}\nнажмите CTRL+C для завершения`)); 
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+// app.listen(8000, function () {
+//   console.log('Express запущен на http://localhost:8000\nнажмите CTRL+C для завершения');
+// });
